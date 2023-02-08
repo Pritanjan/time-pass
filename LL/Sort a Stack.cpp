4fadc8179a06b916fa351a0c4b9b6450d6cce888
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-09 02:35:06 +0530

    Update Sort a Stack.cpp

--- LL/Sort a Stack.cpp
+++ LL/Sort a Stack.cpp
@@ -1,3 +1,6 @@
+// https://www.codingninjas.com/codestudio/problems/sort-a-stack_985275?leftPanelTab=0
+// https://leetcode.com/discuss/interview-question/algorithms/125398/given-a-stack-sort-it-in-non-decreasing-order
+
 #include <bits/stdc++.h> 
 void sortedInsert(stack<int> &stk, int temp){
 
@@ -31,3 +34,7 @@ void sortStack(stack<int> &stk) {
 
 // T.C. each element is popped out elements from stack & insert in the other one. which takes O(N) time .
 // So for n elemenets it will be O(N^2)
+
+// S.C. O(N)
+
+
