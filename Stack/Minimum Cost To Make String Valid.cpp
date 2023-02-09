b8ae1f2a0dc66db301556177c76a604f9e677f97
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-09 09:17:06 +0530

    Create Minimum Cost To Make String Valid.cpp

---
+++ Stack/Minimum Cost To Make String Valid.cpp
@@ -0,0 +1,35 @@
+// cnt the no of opening & closing braces
+
+#include <bits/stdc++.h> 
+int findMinimumCost(string s) {
+  if(s.size() % 2 != 0) return -1;
+
+  stack<char> stk;
+
+  for(int i=0; i<s.size(); i++){
+    char ch = s[i];
+
+    if(ch == '{') stk.push(ch);
+    else{
+      if(!stk.empty() && stk.top() == '{') stk.pop();
+      else stk.push(ch);
+    }
+  }
+
+  
+  int p = 0, l = 0;
+  while(!stk.empty()){
+    if(stk.top() == '{') p++;
+    else l++;
+ 
+    stk.pop();
+  }
+    
+  int res = (p+1)/2 + (l+1)/2;
+
+
+  return res;
+}
+
+
+
