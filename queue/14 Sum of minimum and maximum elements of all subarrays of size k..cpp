d5bfbc05cc5b359a1f41973e3fb5e6daa623b5a7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-13 17:30:08 +0530

    Update 14 Sum of minimum and maximum elements of all subarrays of size k..cpp

--- queue/14 Sum of minimum and maximum elements of all subarrays of size k..cpp
+++ queue/14 Sum of minimum and maximum elements of all subarrays of size k..cpp
@@ -1,2 +1,60 @@
 // https://www.geeksforgeeks.org/sum-minimum-maximum-elements-subarrays-size-k/
 
+// https://www.geeksforgeeks.org/sum-minimum-maximum-elements-subarrays-size-k/
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int solve(int *v, int n, int k){
+    std::deque<int> mx(k);;
+    std::deque<int> mi(k);;
+    
+    // addition of first k size window
+    for(int i=0; i<k; i++){
+        while(!mx.empty() && v[mx.back()] <= v[i]) mx.pop_back();
+        
+        while(!mi.empty() && v[mi.back()] <= v[i]) mi.pop_back();
+        
+        mx.push_back(i);
+        mi.push_back(i);
+        
+    }
+    
+    // 
+    int res = 0;
+    for(int i=k; i<n; i++){
+        res += v[mx.front()] + v[mi.front()];
+        
+        // next windoww
+        while(!mx.empty() && i - mx.front() >= k) mx.pop_front();
+        while(!mu.empty() && i - mi.front() >= k) mi.pop_front();
+        
+        // addition
+        while(!mx.empty() && v[mx.back()] <= v[i]) mx.pop_back();
+        
+        while(!mi.empty() && v[mi.back()] <= v[i]) mi.pop_back();
+        
+        mx.push_back(i);
+        mi.push_back(i);
+    }
+    
+    // consider lst window
+}
+
+int main() {
+	int v[7] = {2,-5, -1, 7, -3, -1, -2};
+	
+	cout << solve(v, 7) << endl;
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
