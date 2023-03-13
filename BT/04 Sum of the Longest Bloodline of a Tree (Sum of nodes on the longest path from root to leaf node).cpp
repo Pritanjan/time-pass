b2f37c6b6cf24049ad79110cf016a0ed578086ed
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-13 22:54:51 +0530

    Create 04 Sum of the Longest Bloodline of a Tree (Sum of nodes on the longest path from root to leaf node).cpp

---
+++ BT/04 Sum of the Longest Bloodline of a Tree (Sum of nodes on the longest path from root to leaf node).cpp
@@ -0,0 +1,30 @@
+// https://practice.geeksforgeeks.org/problems/sum-of-the-longest-bloodline-of-a-tree/1
+
+class Solution{
+public:
+
+    int maxSum = INT_MIN;
+    int maxLen = 0;
+
+    void dfs(Node* node, int level, int sum) {
+        if (node == NULL) return;
+        
+        if (node->left == NULL && node->right == NULL) {
+            if (level > maxLen || (level == maxLen && sum + node->data > maxSum)) {
+                maxLen = level;
+                maxSum = sum + node->data;
+            }
+            return;
+        }
+    
+        dfs(node->left, level+1, sum+node->data);
+        dfs(node->right, level+1, sum+node->data);
+    }
+
+    int sumOfLongRootToLeafPath(Node* root) {
+        if (root == NULL) return 0;
+        dfs(root, 0, 0);
+        return maxSum;
+    }
+};
+
