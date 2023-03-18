c9b8da287b8fd404d4d4898c59f4e2ec96cbb2d7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-18 11:42:03 +0530

    Update and rename 02 700 Search in a Binary Search Tree to 02 700 Search in a Binary Search Tree.cpp

--- BST/02 700 Search in a Binary Search Tree.cpp
+++ BST/02 700 Search in a Binary Search Tree.cpp
@@ -1,12 +1,15 @@
 // https://github.com/Pritanjan/Leetcode/blob/main/0601%20-%200700/700.%20Search%20in%20a%20Binary%20Search%20Tree%201%20TREE%20BST%20BT.cpp
 
+// INORDER --> SORTED ORDER
+
+
             avg     skew 
 insertion   O(H)     O(N)
 
 search      O(H)     O(N)  
 
 
-skew 
+SKEW TREE
 
 2
   \
