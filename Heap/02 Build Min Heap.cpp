5f3c2be8eb0214769f1f1791c84650f0bfc2813c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-20 15:51:06 +0530

    Create 02 Build Min Heap.cpp

---
+++ Heap/02 Build Min Heap.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+void heapify(vector<int> &p, int n, int i){
+    int smallest = i;
+    int L = 2*i + 1;
+    int R = 2*i + 2;
+    
+    if(L < n && p[smallest] > p[L]) smallest = L;
+    if(R < n && p[smallest] > p[R]) smallest = R;
+    
+    if(smallest != i){
+        swap(p[smallest], p[i]);
+        heapify(p, n, smallest);
+    }
+}
+
+
+vector<int> buildMinHeap(vector<int> &arr)
+{
+    int n = arr.size();
+    for(int i=n/2-1; i>=0; i--) {
+        heapify(arr, n, i);
+    }
+
+    return arr;
+}
