321762abd70c9ff594a960a19b7e07d857e8b81c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-20 20:36:25 +0530

    Create 06 Minimum Cost of ropes.cpp

---
+++ Heap/06 Minimum Cost of ropes.cpp
@@ -0,0 +1,60 @@
+// Minimum Cost of ropes
+// https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        priority_queue<long long, vector<long long>, greater<long long> > pq;
+        for(int i=0; i<n; i++) pq.push(arr[i]);
+        long long cost = 0;
+        
+        while(pq.size() > 1){
+            long long p = pq.top();
+            pq.pop();
+            
+            long long o = pq.top();
+            pq.pop();
+            
+            long sum = p + o;
+            cost += sum;
+            
+            pq.push(sum);
+        }
+        return cost;
+    }
+};
+
+// S.C. --> O(N)
+// T.C. --> O(N log N)
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long i, a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.minCost(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+
