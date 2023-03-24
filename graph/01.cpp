f63f820f7fe503d47264e7f046631004369037ff
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-24 09:21:26 +0530

    Update and rename New Text Document.txt to 01.cpp

---
+++ graph/01.cpp
@@ -0,0 +1,70 @@
+// Graph creation
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin>>n>>m;
+	
+	// graph here
+	int adj[n+1][m+1];
+	for(int i=0; i<n; i++){
+	    int u, v;
+	    cin >> u >> v;
+	    adj[u][v] = 1;
+	    adj[v][u] = 1;
+	}
+	return 0;
+}
+
+// T.C. --> O(N)
+
+
+
+
+// UNODIERETED GRaPH
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin>>n>>m;
+	
+	vector<int> adj[n+1];
+	for(int i=0; i<m; i++){
+	    int u, v;
+	    cin >> u >> v;
+	    adj[u].push_back(v);
+	    adj[v].push_back(u);
+	}
+	return 0;
+}
+
+// T.C. --> O(N)
+
+
+
+
+// UDIERETED GRaPH ( FROM u ---> v )
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin>>n>>m;
+	
+	vector<int> adj[n+1];
+	for(int i=0; i<m; i++){
+	    int u, v;
+	    cin >> u >> v;
+	    adj[u].push_back(v);
+      // u --> v
+	    // adj[v].push_back(u);
+	}
+	return 0;
+}
+
+
