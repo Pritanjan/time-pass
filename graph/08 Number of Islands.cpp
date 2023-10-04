f52b8aa387f87189a83af8549765b6bcecfe51cf
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-04 09:43:50 +0530

    Create 08 Number of Islands.cpp

---
+++ graph/08 Number of Islands.cpp
@@ -0,0 +1,6 @@
+// https://practice.geeksforgeeks.org/problems/find-the-number-of-islands/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+// LC  - 270
+// Number of Islands
+
+// https://github.com/Pritanjan/Leetcode/blob/main/0101%20-%200200/200.%20Number%20of%20Islands.cpp
