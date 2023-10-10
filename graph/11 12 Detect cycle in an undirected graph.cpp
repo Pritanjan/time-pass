eeb0fbeb91064e9b677d58593e79fd8c4eeac56b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-10 16:21:48 +0530

    Update and rename 11 Detect cycle in an undirected graph.cpp to 11 12 Detect cycle in an undirected graph.cpp

--- graph/11 12 Detect cycle in an undirected graph.cpp
+++ graph/11 12 Detect cycle in an undirected graph.cpp
@@ -1,5 +1,7 @@
 // https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 
+// BFS
+
 class Solution {
   private:
   bool detect(int src, vector<int> adj[], int vis[]) {
@@ -37,3 +39,47 @@ class Solution {
         return false;
     }
 };
+
+
+
+
+
+// G 12 
+
+// DFS
+
+class Solution {
+private:
+    bool dfs(int node, int parent, int vis[], vector<int> adj[]) {
+        vis[node] = 1;
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                if(dfs(it, node, vis, adj) == true) {
+                    return true;
+                }
+            }
+            // i.e. element other than parent already vis here 
+            // it shows there is a cycle in it 
+            else if(it != parent) return true;
+        }
+        return false;
+    }
+  public:
+    // It will work for single component graph
+    // bool isCycle(int V, vector<int> adj[]) {
+    //     int vis[V] = {0};
+    //     return dfs(1, -1, vis, adj);
+    // }
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        int vis[V] = {0};
+        for(int i=0; i<V; i++) {
+            if(!vis[i]) {
+                if(dfs(i, -1, vis, adj)) return true;
+            }
+        }
+        return false;
+    }
+};
+
+
